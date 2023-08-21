dac927e17b40400b97a22197950938ee118a6fac
nna01 <arony.nowshin@gmail.com>
2023-08-21 12:45:44 -0700

    finished auth flow

---
+++ models/User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose; //Destructuring
+
+const userSchema = new Schema({
+
+    googleId: String
+});
+
+mongoose.model('users', userSchema);
