116a3b2426efdaa6c4c201e1b63c6baf42faff7f
nna01 <arony.nowshin@gmail.com>
2023-08-21 13:12:52 -0700

    tweak google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -23,6 +23,7 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       //Find the first ID that matches the profile id, it returns a promise
